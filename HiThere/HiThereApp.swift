0eb175325ed961b6b5e3c52bf5618178ba1e8413
Carlito Kabambi <carlito.kabambi@triosstudent.com>
2025-03-13 15:54:56 -0400

    Initial Commit

---
+++ HiThere/HiThereApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiThereApp.swift
+//  HiThere
+//
+//  Created by Royal K on 2025-03-13.
+//
+
+import SwiftUI
+
+@main
+struct HiThereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HiThereTests/HiThereTests.swift
@@ -0,0 +1,17 @@
+//
+//  HiThereTests.swift
+//  HiThereTests
+//
+//  Created by Royal K on 2025-03-13.
+//
+
+import Testing
+@testable import HiThere
+
+struct HiThereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
